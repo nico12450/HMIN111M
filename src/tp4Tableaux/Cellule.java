89872fb765732d2c1fbe41f8ac1cfbb52aaf2342
nicolas.gamel12450@gmail.com <nicolas.gamel12450@gmail.com>
2018-10-13 10:23:24 +0200

    ajout des attributs et méthodes de la classe cellule

--- src/tp4Tableaux/Cellule.java
+++ src/tp4Tableaux/Cellule.java
@@ -1,9 +1,89 @@
 package tp4Tableaux;
 
 public class Cellule {
+	
+	private boolean occupee;
+	private int age;
+	private Monde monMonde;
+	private int ligne, colonne;
 
-	public Cellule() {
-		// TODO Auto-generated constructor stub
+	public Cellule(boolean occupee, Monde monMonde, int ligne, int colonne) {
+		
+		this.setOccupee(occupee);
+		this.monMonde = monMonde;
+		this.ligne = ligne;
+		this.colonne = colonne;
+		
 	}
+	
+	public void setOccupee(boolean b) {
+		
+		this.occupee = b;
+		this.age = 0;
+		
+		if(!b) {
+			
+			this.age = -1;
+			
+		}
+		
+	}
+	
+	public boolean estVivante() {
+		return this.occupee;
+	}
+	
+	public int[] getPosition() {
+		return new int[] {this.ligne,this.colonne};
+	}
+	
+	public void meurt() {
+		this.setOccupee(false);
+	}
+	
+	public void nait() {
+		this.setOccupee(true);
+	}
+	
+	public void prochainEtat(int nbVoisins) {
+		
+		if(this.estVivante() && (nbVoisins == 2 || nbVoisins == 3)) {
+			
+			this.age += 1;
+			
+		}
+		
+		else if(this.estVivante()) {
+			this.meurt();
+		}
+		
+		else if(nbVoisins == 3) {
+			this.nait();
+		}
+		
+	}
+	
+	public char etat() {
+		return this.estVivante() ? 'O' : 'X';
+	}
+	
+	public String toString() {
+		
+		String description = "\n";
+		
+		if(this.estVivante()) {
+			description += "la cellule est vivante depuis " + this.age + " générations\n";
+		}
+		
+		else {
+			description += "la cellule est morte\n";
+		}
+		
+		description += "elle est en position " + this.getPosition()[0] + " , " + this.getPosition()[1] + "\n";
+		
+		return description;
+		
+	}
+
 
 }
